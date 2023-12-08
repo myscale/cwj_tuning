0bd81851eeda958d52f6a16ebb1b4de81205da54
steven-ccq <55176896+steven-ccq@users.noreply.github.com>
2023-12-08 09:24:47 +0800

    Update train_sft.sh

--- train_sft.sh
+++ train_sft.sh
@@ -1,18 +1,20 @@
 CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --stage sft \
-    --model_name_or_path /data/ccq/models/Baichuan2-13B-Chat \
+    --model_name_or_path YOUR_BASE_MODEL_PATH \
     --do_train \
-    --dataset Baichuan2_characters \
+    --dataset YOUR_TRAIN_DATASET_NAME \
+    --template default \
     --finetuning_type lora \
-    --output_dir output/Baichuan2_characters \
+    --lora_target all \
+    --output_dir YOUR_OUTPUT_DIR \
     --overwrite_cache \
-    --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --per_device_train_batch_size 6 \
+    --gradient_accumulation_steps 16 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
-    --save_steps 1000 \
+    --save_steps 200 \
     --learning_rate 5e-5 \
-    --num_train_epochs 2.0 \
+    --num_train_epochs 3.0 \
     --plot_loss \
     --fp16 \
     --quantization_bit 4
